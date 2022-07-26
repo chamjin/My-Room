9897ccc0a6b4a1dae20f7a020799768549788f4a
chamjin <playingme@naver.com>
2022-07-26 21:31:08 +0900

    220726

---
+++ 220726.py
@@ -0,0 +1,16 @@
+# 백준 1929 (M이상 N이하의 소수 출력)
+
+def isPrime(num):
+    if num==1:
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
+                return False
+        return True
+
+M, N = map(int, input().split())
+
+for i in range(M, N+1):
+    if isPrime(i):
+        print(i)
